7109f005a99a74d85020c19ffe34077bbe908806
Kevin del Cid <danieldelcidf@gmail.com>
2022-09-30 23:42:41 -0600

    solucion de los productos recomendados

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Carousel } from "react-bootstrap";
+import { Button, Card, Carousel, Col, Row } from "react-bootstrap";
 import { useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router";
 import "../styles/products.css";
@@ -9,16 +9,59 @@ const Product = () => {
 
   const products = useSelector((state) => state.products);
   const product = products.find((prod) => prod.id === Number(id));
-  const [relatedProducts, setRelatedProducts] = useState([]);
+
   // console.log(product);
 
-  const getRelatedPorducts = products.filter(
+  const relatedPorducts = products.filter(
     (filtrateProduct) => filtrateProduct.category.id === product.category.id
   );
-  console.log(getRelatedPorducts);
 
+  const generateRelationSearch = () => {
+    const forFirstLetters = products.filter((filtrateProduct) =>
+      filtrateProduct.title
+        .toLowerCase()
+        .includes(product.title.toLowerCase().substring(0, 4))
+    );
+
+    const forMiddleLetters = products.filter((filtrateProduct) =>
+      filtrateProduct.title
+        .toLowerCase()
+        .includes(
+          product.title
+            .toLowerCase()
+            .substring(
+              Math.floor(filtrateProduct.title.length / 2),
+              Math.floor(filtrateProduct.title.length / 2) + 4
+            )
+        )
+    );
+
+    const forLastLetters = products.filter((filtrateProduct) =>
+      filtrateProduct.title
+        .toLowerCase()
+        .includes(
+          product.title
+            .toLowerCase()
+            .substring(
+              filtrateProduct.title.length - 5,
+              product.title.length - 1
+            )
+        )
+    );
+
+    const generatedResults = [
+      ...forFirstLetters,
+      ...forMiddleLetters,
+      ...forLastLetters,
+    ];
+    const result = generatedResults.filter(
+      (item, index) => generatedResults.indexOf(item) === index
+    );
+    return result;
+  };
   return (
     <div>
+      <h1>{product?.title}</h1>
       <Carousel className="carrousel-container">
         {product?.productImgs.map((img) => (
           <Carousel.Item interval={2000} key={img}>
@@ -35,11 +78,57 @@ const Product = () => {
         ))}
       </Carousel>
       <section>
-        {getRelatedPorducts.map((prod) => (
-          <button type="" onClick={() => navigate(`/product/${prod.id}`)}>
-            <b>{prod.title}</b>
-          </button>
-        ))}
+        <h2>Articulos realcionados</h2>
+        <Row>
+          {relatedPorducts.map((prod) => (
+            // <button type="" onClick={() => navigate(`/product/${prod.id}`)}>
+            //   <b>{prod.title}</b>
+            // </button>
+
+            <Col>
+              <Card
+                onClick={() => navigate(`/product/${prod.id}`)}
+                style={{ width: "18rem" }}
+              >
+                <Card.Img
+                  variant="top"
+                  className="img-product-selected"
+                  src={prod?.productImgs}
+                />
+                <Card.Body>
+                  <Card.Title>{prod.title}</Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </section>
+
+      <section>
+        <h2>Quizás te interese</h2>
+        <Row>
+          {generateRelationSearch().map((prod) => (
+            // <button type="" onClick={() => navigate(`/product/${prod.id}`)}>
+            //   <b>{prod.title}</b>
+            // </button>
+
+            <Col>
+              <Card
+                onClick={() => navigate(`/product/${prod.id}`)}
+                style={{ width: "18rem" }}
+              >
+                <Card.Img
+                  variant="top"
+                  className="img-product-selected"
+                  src={prod?.productImgs}
+                />
+                <Card.Body>
+                  <Card.Title>{prod.title}</Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
       </section>
     </div>
   );
