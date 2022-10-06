878f27eba7c258ea88647a6c5c17e3feabb3d007
PC Work <danieldelcidf@gmail.com>
2022-10-06 16:12:32 -0600

    see agrego la vista protegida de profile y su funcionalidad

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router';
+import { deleteCart } from '../store/slices/cart.slice';
+import { setUser } from '../store/slices/user.slice';
 
 const Profile = () => {
+
+
+    const user = useSelector(state => state.user)
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    const logout = () => {
+        localStorage.removeItem("user");
+        localStorage.removeItem("token");
+        dispatch(setUser({}));
+        dispatch(deleteCart());
+        navigate("/");
+      };
+    
+
     return (
         <div>
-            
+            <h1>Bienvenido {user.user.firstName} {user.user.lastName}</h1>
+
+            {user.user.email}
+            {user.user.phone}
+            {user.user.role}
+            {user.user.status}
+
+
+            <button onClick={logout}>Logout</button>
         </div>
     );
 };
